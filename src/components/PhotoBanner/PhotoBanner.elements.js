e158ae86360adeb732c49d4f6955615ac0a54dd1
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-30 15:56:45 -0700

    add banner image at top of page that stays static as you scrollpast

---
+++ src/components/PhotoBanner/PhotoBanner.elements.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const PhotoBannerSec = styled.div`
+top: 0;
+  position: sticky;
+  width: 100%;
+  border-bottom: 1px solid black;
+  border-top: 1px solid black;
+  z-index: -1;
+`;
+
+export const BannerImg = styled.img`
+  width: 100%;
+  z-index: -1
+`;
