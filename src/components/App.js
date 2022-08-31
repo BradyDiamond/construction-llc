0e27d7df41664b7c08098bccd100d6a86d662003
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-31 11:24:43 -0700

    add services path/route

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Home from './pages/Home/Home';
+import Services from './pages/Services/Services';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' exact element={<Home />} />
+          <Route path='/services' exact element={<Services />} />
         </Routes>
         <Footer />
       </Router>
