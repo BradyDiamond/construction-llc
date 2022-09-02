6b664475aec5fc9a2bf3110ba2270c36bb67a228
delcidj22 <jdelcid23@gmail.com>
2022-09-01 21:37:33 -0700

    Create and Update gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,12 +1,47 @@
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from "react";
+import './Gallery.css';
+import Img1 from './Images/contractors1.jpg';
+import Img2 from './Images/contractors2.jpg';
+import Img3 from './Images/contractors3.jpg';
+import Img4 from './Images/contractors4.jpg';
 
 function Gallery() {
+
+  let data = [
+    {
+      id: 1,
+      imgSrc: Img1,
+    },
+    {
+      id: 2,
+      imgSrc: Img2
+    },
+    {
+      id: 3,
+      imgSrc: Img3
+    },
+    {
+      id: 4,
+      imgSrc: Img4
+    },
+  ]
   return (
+    <>
     <h1>Testing Gallery</h1>
+  
+    <div className="gallery">
+      {data.map((item, index)=> {
+        return(
+          <div className="pics" key={index}>
+            <img src={item.imgSrc} style ={{width: '100%' }}/>
+          </div>
+        )
+      })}
+    </div>
+    
+    </>
+
+    
 
   )
   }
